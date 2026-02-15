6701dbda80bfbee86f6e1dc617c7c973c1bd98a2
Diego Andrés Muñoz Morales <86175879+DimmoX@users.noreply.github.com>
2026-02-15 02:42:08 -0300

    feat: configurar conexión a RabbitMQ y Oracle
    - Agregar application.yml con configuración de datasource
    - Configurar propiedades de RabbitMQ
    - Agregar punto de entrada ConsumerSchedulesApplication

---
+++ src/main/java/com/busesred/consumer/schedules/ConsumerSchedulesApplication.java
@@ -0,0 +1,11 @@
+package com.busesred.consumer.schedules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerSchedulesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerSchedulesApplication.class, args);
+    }
+}
